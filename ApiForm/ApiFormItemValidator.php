7fa7884354ad69439a2b28e9b12b091c839ec6d5
hanwoolderink <hanwoolderink@gmail.com>
2020-08-25 07:28:35 +0200

    added phpcs

--- ApiForm/ApiFormItemValidator.php
+++ ApiForm/ApiFormItemValidator.php
@@ -138,7 +138,8 @@ class ApiFormItemValidator
     {
         $d = DateTime::createFromFormat($format, $date);
 
-        // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+        // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits
+        // so changing the comparison from == to === fixes the issue.
         return $d && $d->format($format) === $date;
     }
 
