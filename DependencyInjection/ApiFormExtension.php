af9dd033ad0acf3c370c5fc1fdb3d6986a85bca2
hanwoolderink <hanwoolderink@gmail.com>
2020-08-26 21:46:23 +0200

    test

--- DependencyInjection/ApiFormExtension.php
+++ DependencyInjection/ApiFormExtension.php
@@ -19,11 +19,17 @@ class ApiFormExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config        = $this->processConfiguration($configuration, $configs);
+
         $loader = new XmlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
         );
 
         $loader->load('request-service.xml');
+
+        $container->setParameter('api_form.test_var', $config['test_var']);
+        $container->setParameter('api_form.test_var2', $config['test_var2']);
     }
 }
